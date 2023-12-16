c5dcab83387422973ff89601aba46e1218ba44c0
LF <lf.herreragaray@gmail.com>
2023-12-16 09:41:45 -0600

    Actualizacion de indentado

--- Drivers/API/src/API_I2C.c
+++ Drivers/API/src/API_I2C.c
@@ -29,7 +29,7 @@ void I2C1_Gpio(void)
 /*@brief I2C_Init: Inicializa el protocolo I2C
  * @param void
  * @return bool_t HAL_OK o HAL_ERROR
-*/
+ */
 bool_t I2C_Init(void)
     {
     I2C1_Gpio();
@@ -55,11 +55,11 @@ bool_t I2C_Init(void)
 /*@brief I2C_device: Busca un dispositivo que conteste a una direccion y la retorna
  * @param void
  * @return uint8_t Addres del dispositivo que está listo
-*/
+ */
 uint8_t I2C_device(void)
     {
-    int i=0;
-    for(;i<128;i++)
+    int i = 0;
+    for (; i < 128; i++)
 	{
 	HAL_I2C_IsDeviceReady(&I2C1_handle, (i) << 1, 1, TIMEOUT);
 	}
@@ -70,39 +70,42 @@ uint8_t I2C_device(void)
 /*@brief I2C_transmit: Transmite dato a un dispositivo
  * @param void
  * @return void
-*/
+ */
 void I2C_transmit(uint8_t addr, uint8_t *p_data, uint8_t size)
     {
     HAL_I2C_Master_Transmit(&I2C1_handle, addr << 1, p_data, size, TIMEOUT);
-
 
     }
 
 /*@brief I2C_transmitMem: transmite datos usando modo bloqueante a una direccion de memoria
  * @param void
  * @return void
-*/
-void I2C_transmitMem(uint8_t addr,uint8_t regAddr, uint8_t *p_data)
+ */
+void I2C_transmitMem(uint8_t addr, uint8_t regAddr, uint8_t *p_data)
     {
-    HAL_I2C_Mem_Write(&I2C1_handle, addr<<1, regAddr, sizeof(regAddr), p_data, sizeof(*p_data), TIMEOUT);
+    HAL_I2C_Mem_Write(&I2C1_handle, addr << 1, regAddr, sizeof(regAddr), p_data,
+	    sizeof(*p_data), TIMEOUT);
 
     }
 
 /*@brief I2C_receiveMem: Lee datos usando modo bloqueante a una direccion de memoria
  * @param void
  * @return uint32_t
-*/
-uint32_t I2C_receiveMem(uint8_t addr,uint8_t regAddr,uint8_t* p_data,uint8_t size)
+ */
+uint32_t I2C_receiveMem(uint8_t addr, uint8_t regAddr, uint8_t *p_data,
+	uint8_t size)
     {
-    HAL_I2C_Mem_Read(&I2C1_handle, addr<<1, regAddr, sizeof(regAddr), p_data,size, TIMEOUT);
+    HAL_I2C_Mem_Read(&I2C1_handle, addr << 1, regAddr, sizeof(regAddr), p_data,
+	    size, TIMEOUT);
     return *p_data;
     }
 
 /*@brief I2C_receive:  Recibe de modo Master una cantidad de datos en modo bloqueante
  * @param void
  * @return void
-*/
+ */
 void I2C_receive(uint8_t addr, uint8_t *p_data)
     {
-    HAL_I2C_Master_Receive(&I2C1_handle, addr << 1, p_data, sizeof(p_data), TIMEOUT);
+    HAL_I2C_Master_Receive(&I2C1_handle, addr << 1, p_data, sizeof(p_data),
+	    TIMEOUT);
     }
